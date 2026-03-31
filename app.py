a26f8230e16f1e92b7d7900939acc956a88ceceb
HassanMirza123 <hassan.mirza@city.ac.uk>
2026-03-31 13:11:45 +0100

    added the side bar slides

--- app.py
+++ app.py
@@ -7,3 +7,21 @@ st.set_page_config(
 st.title("A&E Discrete Event Simulation")
 st.markdown("Explores how changes to staffing levels affect patient wait times and performance targets.")
 
+#Param sliders on the sidebar
+st.sidebar.header("Change params")
+
+st.sidebar.markdown("**Baseline:** 6 nurses, 13 doctors")
+
+n_nurses = st.sidebar.slider(
+    label="Number of Triage Nurses", min_value=1, max_value=15, value=6, step=1
+)
+
+n_doctors = st.sidebar.slider(
+    label="Number of Doctors", min_value=1, max_value=25, value=13, step=1
+)
+
+n_reps = st.sidebar.slider(
+    label="Replications (higher = more accurate, but slower)",min_value=5,max_value=30,value=10
+)
+
+run_button = st.sidebar.button("▶ Run Simulation", type="primary")
