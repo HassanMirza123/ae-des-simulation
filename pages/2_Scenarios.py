f67f146af306503fb9d72cbbd25370206df939fb
HassanMirza123 <hassan.mirza@city.ac.uk>
2026-04-06 14:21:51 +0100

    Adding placeholder pages (Need to do scenario comparison)

---
+++ pages/2_Scenarios.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Scenarios - A&E DES",
+    layout="wide"
+)
+
+st.title("Scenario Comparison")
---
+++ pages/3_About.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="About - A&E DES",
+    layout="wide"
+)
+
+st.title("About this tool")
+st.markdown("---")
+
+st.markdown("""
+Need to talk about data sources, all assumptions, reiterate its just a proof of concept, how sim was calibrated, anything else I can think of, maybe image of my sim plan
+""")
